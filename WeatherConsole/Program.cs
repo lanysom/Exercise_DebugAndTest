70ffc9c63d575ef5afb931fb04154405a6b237a9
Lars Nysom <lars.nysom@gmail.com>
2020-09-09 12:37:42 +0200

    Implemented main method

--- WeatherConsole/Program.cs
+++ WeatherConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Meteorology;
+using System;
 
 namespace WeatherConsole
 {
@@ -9,6 +10,17 @@ namespace WeatherConsole
             // TODO: Use the Meteorology project to calculate wind chill factor.
             //       Write the result in the console window.
             //       Check the results with the online calculation tool and debug the program if the results do not comply.
+
+            double inputDegree = 5.0;
+            double inputSpeed = 9.0;
+
+            WindChillFactor windChillFactor = new WindChillFactor(SpeedUnit.MeterPerSecond, TemperatureScale.Celsius);
+            double result = windChillFactor.CalulateChillFactorTemperature(inputDegree, inputSpeed);
+
+            // Print input values and resulting temperature.
+            Console.WriteLine("Temperature: {0}", inputDegree);
+            Console.WriteLine("Wind: {0}", inputSpeed);
+            Console.WriteLine("Chill Factor: {0}", result);
         }
     }
 }
