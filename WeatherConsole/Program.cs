a0343d15abf9fb48ade3a3797ea448f64830feb3
Lars Nysom <lars.nysom@gmail.com>
2020-09-09 12:36:12 +0200

    Updated TODO in main

--- WeatherConsole/Program.cs
+++ WeatherConsole/Program.cs
@@ -6,7 +6,9 @@ namespace WeatherConsole
     {
         static void Main(string[] args)
         {
-            // TODO: Use the Meteorology project to calculate wind chill factor. Check the results with the online calculation tool and debug the program if the results do not comply.
+            // TODO: Use the Meteorology project to calculate wind chill factor.
+            //       Write the result in the console window.
+            //       Check the results with the online calculation tool and debug the program if the results do not comply.
         }
     }
 }
